e83961c4129b56dc58bc145a9ed9f6a5b29987e9
Kemati <kemati.morgan@mymona.uwi.edu>
2017-04-04 04:22:12 +0000

    Exercise 3

--- image-getter.py
+++ image-getter.py
@@ -2,29 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
-url = "https://www.walmart.com/ip/TCL-50FS3800-50-1080p-120Hz-Smart-LED-HDTV-with-Roku/45100428"
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
-
-# This will look for a meta tag with the og:image property
-og_image = (soup.find('meta', property='og:image') or
-                    soup.find('meta', attrs={'name': 'og:image'}))
-if og_image and og_image['content']:
-    print og_image['content']
-    print ''
-
-# This will look for a link tag with a rel attribute set to 'image_src'
-thumbnail_spec = soup.find('link', rel='image_src')
-if thumbnail_spec and thumbnail_spec['href']:
-    print thumbnail_spec['href']
-    print ''
-
-
-image = """<img src="%s"><br />"""
-for img in soup.findAll("img", src=True):
-   print image % urlparse.urljoin(url, img["src"])
-   print ''
+def image_getter():
+    url = "https://www.walmart.com/ip/TCL-50FS3800-50-1080p-120Hz-Smart-LED-HDTV-with-Roku/45100428"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    
+    # This will look for a meta tag with the og:image property
+    og_image = (soup.find('meta', property='og:image') or
+                        soup.find('meta', attrs={'name': 'og:image'}))
+    if og_image and og_image['content']:
+        print og_image['content']
+        print ''
+    
+    # This will look for a link tag with a rel attribute set to 'image_src'
+    thumbnail_spec = soup.find('link', rel='image_src')
+    if thumbnail_spec and thumbnail_spec['href']:
+        print thumbnail_spec['href']
+        print ''
+    
+    
+    image = []
+    image = """<img src="%s"><br />"""
+    for img in soup.findAll("img", src=True):
+       print image % urlparse.urljoin(url, img["src"])
+       print ''
+    return image
    
    
    
-def
+
